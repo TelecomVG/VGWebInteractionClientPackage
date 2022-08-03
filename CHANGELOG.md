0935840c39ab5d78da0f65fa413a5701a376f904
Guillermo Lucio <guillermo.lucio@gmail.com>
2022-08-02 21:47:32 -0500

    - Bugfix Speaker

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,10 @@ No es necesario actualizar la libreria WebRTC
 
 ### Fixed
 - Se agregaron las solicitudes de acceso para cámara y micrófono ya que solo se estaba preguntando si tenian el permiso habilitado o no.
+
+## [1.0.3] - 2022-08-02
+No es necesario actualizar la libreria WebRTC
+
+### Fixed
+- Se habilitó el speaker en automático para la videollamada cuando no hay audífonos conectados.
+- Durante la videollamada, se ruteara el audio en automático cuando se conecten y desconecten audífonos. 
--- Package.swift
+++ Package.swift
@@ -10,11 +10,13 @@ let package = Package(
             name: "VGWebInteractionClientSDK",
             targets: ["VGWebInteractionClientSDK"]),
     ],
-    dependencies: [],
+//    dependencies: [
+//		.package(url: "https://github.com/TelecomVG/WebRTC.git", .upToNextMinor(from: "102.5005.0"))
+//	],
     targets: [
 		.binaryTarget(
 			name: "VGWebInteractionClientSDK",
-			url: "https://telecom.vg/assets/videoroom/mobile/ios/releases/1.0.2/VGWebInteractionClientSDK.xcframework.zip",
-			checksum: "b9bb5cffcd8b2eb14b2007ee96b08648a523676dccaf1aa0915f07a219a09108")
+			url: "https://telecom.vg/assets/videoroom/mobile/ios/releases/1.0.3/VGWebInteractionClientSDK.xcframework.zip",
+			checksum: "abec3cd39c8211f014e51fdf2a8ea92593cbf1293d6234974333d3a83cff3d6b")
     ]
 )
